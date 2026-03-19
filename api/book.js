191ade3bd5fb1e9e1598ec2ba41f0f043e5dd324
SwiftSites <swiftsitesnz@gmail.com>
2026-03-19 16:57:03 +1300

    Fix api/book.js - use CommonJS exports for Vercel compatibility

--- api/book.js
+++ api/book.js
@@ -19,7 +19,7 @@ async function sendEmail(to, subject, html) {
   return res.json();
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
@@ -78,4 +78,4 @@ export default async function handler(req, res) {
     console.error(err);
     return res.status(500).json({ error: 'Failed to send email' });
   }
-}
+};
